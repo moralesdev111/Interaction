d2196eb29a2f39023fbb7144cf353e58270ccbb2
moralesdev111 <moralesdev111@gmail.com>
2023-06-06 00:57:51 +0900

    Dialogue Editor

--- Interactions Prototype/Assets/Dialogue/Editor/DialogueEditor.cs
+++ Interactions Prototype/Assets/Dialogue/Editor/DialogueEditor.cs
@@ -5,22 +5,66 @@ using UnityEditor.Callbacks;
 using UnityEditor;
 
 namespace RPG.Dialogue.Editor
-{
-    
+{    
     public class DialogueEditor : EditorWindow 
     {
+    Dialogue selectedDialogue = null;
+
     [MenuItem("Window/Dialogue Editor")]
     public static void ShowEditorWindow()
     {
-
         GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
     }
     
-    [OnOpenAssetAttribute(1)]
+    [OnOpenAsset(1)]
     public static bool OnOpenAsset(int instanceID, int line)
     {
-        Debug.Log("OpenDialogue");
+        Dialogue dialogue = EditorUtility.InstanceIDToObject(instanceID) as Dialogue;
+        if (dialogue != null)
+        {
+            ShowEditorWindow();
+            return true;
+        }
+        
         return false;
+    }
+    void OnEnable() 
+    {
+    Selection.selectionChanged += OnSelectionChanged;
+
+    }
+    
+    void OnSelectionChanged() 
+    {
+        Dialogue newDialogue = Selection.activeObject as Dialogue;
+        if (newDialogue != null)
+        {
+            selectedDialogue = newDialogue;
+            Repaint();
+        }
+    }
+
+    void OnGUI() 
+    {
+        if (selectedDialogue == null)
+    {
+        EditorGUILayout.LabelField("No Dialogue Selected");
+    }
+    else
+    {
+        foreach (DialogueNode node in selectedDialogue.GetAllNodes())
+        {
+            EditorGUILayout.LabelField(node.text);
+
+        }
+    }          
+
+
+{
+    
+}
+
+                   
     }
     }
 }
