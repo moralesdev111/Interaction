813e58f418103beccc8d511faab188068a0c10a4
moralesdev111 <moralesdev111@gmail.com>
2023-06-07 09:22:53 +0900

    Dialogue 3 part

--- Interactions Prototype/Assets/Dialogue/Editor/DialogueEditor.cs
+++ Interactions Prototype/Assets/Dialogue/Editor/DialogueEditor.cs
@@ -21,7 +21,15 @@ namespace RPG.Dialogue.Editor
     [NonSerialized]
     DialogueNode deletingNode = null; 
     [NonSerialized]
-    DialogueNode linkingParentNode = null;    
+    DialogueNode linkingParentNode = null;
+    Vector2 scrollPosition; 
+    [NonSerialized]
+    bool draggingCanvas = false;
+    [NonSerialized]
+    Vector2 draggingCanvasOffset;
+
+    const float canvasSize = 4000; 
+    const float backgroundSize = 50;  
    
 
     [MenuItem("Window/Dialogue Editor")]
@@ -72,6 +80,13 @@ namespace RPG.Dialogue.Editor
     else
         {
         ProcessEvents();
+
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition);
+        
+        Rect canvas = GUILayoutUtility.GetRect(canvasSize,canvasSize);
+        Texture2D backgroundText = Resources.Load("background") as Texture2D;
+        Rect textCoords = new Rect(0,0, canvasSize / backgroundSize, canvasSize / backgroundSize);
+        GUI.DrawTextureWithTexCoords(canvas, backgroundText, textCoords);
         
         foreach (DialogueNode node in selectedDialogue.GetAllNodes())
                 {                   
@@ -81,6 +96,9 @@ namespace RPG.Dialogue.Editor
                 {
                     DrawNode(node);                    
                 }  
+
+                EditorGUILayout.EndScrollView();
+                
                 if (creatingNode !=null)
                 {
                     Undo.RecordObject(selectedDialogue, "Added Dialogue Node");
@@ -103,23 +121,37 @@ namespace RPG.Dialogue.Editor
     {
         if(Event.current.type == EventType.MouseDown && draggingNode == null)
         {
-            draggingNode = GetNodeAtPoint(Event.current.mousePosition);
+            draggingNode = GetNodeAtPoint(Event.current.mousePosition + scrollPosition);
             if (draggingNode !=null)
             {
                 draggingOffset = draggingNode.rect.position - Event.current.mousePosition;
-
+            }
+            else
+            {
+                draggingCanvas = true;
+                draggingCanvasOffset = Event.current.mousePosition + scrollPosition;
             }
         }
         else if(Event.current.type == EventType.MouseDrag && draggingNode !=null)
         {
             Undo.RecordObject(selectedDialogue, "Move Dialogue Node");
             draggingNode.rect.position = Event.current.mousePosition + draggingOffset;
+
+            GUI.changed = true;
+        }
+        else if(Event.current.type == EventType.MouseDrag && draggingCanvas)
+        {
+            scrollPosition = draggingCanvasOffset - Event.current.mousePosition;
             GUI.changed = true;
         }
         else if(Event.current.type == EventType.MouseUp && draggingNode !=null)
         {
             draggingNode = null;
             
+        }
+        else if(Event.current.type == EventType.MouseUp && draggingCanvas)
+        {
+            draggingCanvas = false;
         }
 
     }
